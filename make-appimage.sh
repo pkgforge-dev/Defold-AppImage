a7f82a31a6d4f7efef13400e9088c11a85517447
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-21 20:13:20 -0400

    likely needed

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/defold.png
 export DESKTOP=/usr/share/applications/Defold.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 mkdir -p ./AppDir/bin
