b37ebdfa03611b5e2f4633d150b8b0e6dbb49daa
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-29 20:21:01 -0400

    add test

--- make-appimage.sh
+++ make-appimage.sh
@@ -32,3 +32,7 @@ quick-sharun ./AppDir/bin/* ./AppDir/bin/packages/jdk*/bin/* ./AppDir/bin/packag
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
