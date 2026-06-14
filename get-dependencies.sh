167e9e6d9510680128a5122ba2e1bb0acf699657
Samueru-sama <xdglawyer@outlook.com>
2026-06-14 19:46:36 -0400

    stop relying on the AUR package

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,31 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm glu
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package defold-bin
+#make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Getting binary..."
+echo "---------------------------------------------------------------"
+
+link=https://github.com/defold/defold/releases/latest/download/Defold-x86_64-linux.tar.gz
+if ! wget --retry-connrefused --tries=30 "$link" -O /tmp/tarball.tar.gz 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+mkdir -p ./AppDir/bin
+tar xfv /tmp/tarball.tar.gz
+mv -v ./Defold/* ./AppDir/bin
+cp -v ./AppDir/bin/logo_blue.png ./AppDir/Defold.png
+cp -v ./AppDir/bin/logo_blue.png ./AppDir/.DirIcon
+rm -rf /tmp/tarball.tar.gz ./Defold
+
+awk -F'/' '/Location:/{print $(NF-1); exit}' /tmp/download.log > ~/version
+
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q defold-bin | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+# example command to get version of application here
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/defold.png
-export DESKTOP=/usr/share/applications/Defold.desktop
 export DEPLOY_OPENGL=1
-
-# Deploy dependencies
-mkdir -p ./AppDir/bin
-cp -r /opt/Defold/* ./AppDir/bin
 
 # this nonsense is needed, otherwise ldd cannot find libraries
 # how does this application work at all??? running ldd on the /opt also shows missing libs
-( 
+(
 	cd ./AppDir/bin/packages/jdk*/lib
 	find ./*/* -type f -name '*.so' -exec ln -s {} ./ \; || :
 )
